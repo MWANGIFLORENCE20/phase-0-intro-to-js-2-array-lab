7f86be0abb348b6970bbbea4a8d06c3b441485bc
florence wangechi <florencewangechim@gmail.com>
2023-03-18 12:46:13 +0300

    Complete Assingment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    cats.push(name);  
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop(name);
+}
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift(name);
+}
+
+function appendCat(name){
+    let newCats =[...cats , name]
+    return newCats;
+}
+
+function prependCat(name){
+    let newCats = [name , ...cats]
+    return newCats;
+}
+
+function removeLastCat(name) {
+    let newCats = cats.slice(0 , -1)
+    return newCats;
+}
+
+function removeFirstCat(name) 
+{
+    let newCats = cats.slice(1)
+    return newCats;
+}
+
+ 
